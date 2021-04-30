e227752aa553b5d6cde90d3028cc80e469a56623
Miłosz Chrząszcz <milchr96@gmail.com>
2021-04-30 16:56:42 +0200

    Dodaj pliki projektów.

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        private Math _math;
+        //SetUp - na początku
+        //TearDown - na końcu
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        //[Ignore("Już nie mam sił. Zrobię go jutro.")]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42917461-a7a4-4d0e-9328-f1948d89b891")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
